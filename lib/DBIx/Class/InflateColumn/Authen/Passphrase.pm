9e5f3125c7a567105f640e48d9860632f28def7d
Florian Ragwitz <rafl@debian.org>
2010-12-08 22:42:08 +0100

    Initial version

---
+++ lib/DBIx/Class/InflateColumn/Authen/Passphrase.pm
@@ -0,0 +1,26 @@
+use strict;
+use warnings;
+
+package DBIx::Class::InflateColumn::Authen::Passphrase;
+
+use Authen::Passphrase;
+use parent 'DBIx::Class';
+
+sub register_column {
+    my ($self, $column, $info, @rest) = @_;
+
+    $self->next::method($column, $info, @rest);
+    return unless my $encoding = $info->{passphrase};
+
+    $self->throw_exception(q['rfc2307' and 'crypt' are the only supported types of passphrase columns])
+        unless $encoding eq 'rfc2307' || $encoding eq 'crypt';
+
+    $self->inflate_column(
+        $column => {
+            inflate => sub { Authen::Passphrase->${\"from_${encoding}"}(shift) },
+            deflate => sub { shift->${\"as_${encoding}"} },
+        },
+    );
+}
+
+1;
